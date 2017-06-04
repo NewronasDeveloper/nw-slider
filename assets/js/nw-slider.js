18a756b2328bf92dc72d74d7e8fae0ba98c7f946
NewronasDeveloper <contacto@newronas.com>
2017-06-04 19:25:45 +0200

    Add readme and code improvements

--- assets/js/nw-slider.js
+++ assets/js/nw-slider.js
@@ -1,110 +1,133 @@
-function nw_slider_lib(nw_slider_number){
+function nw_slider_lib(){
 
-	var self = this;
-	var speed;
-	var transition_success = true;
+	var btnActive,
+		self = this,
+		speed,
+		transition,
+		transition_success = true;
 
+	this.btnStatus = true;
 	this.first_left = false;
 	this.first_right = false;
 	this.nw_slider_count = 0;
 	this.nw_elements;
-	this.transition;
+	this.nw_elements_length;
 
 	this.run = function(config){
 
 		this.nw_elements = document.querySelectorAll('#' + config['id'] + ' ul li');
-		this.transition = ((config['transition']!=undefined)?config['transition']:'linear-left');
-		speed = ((config['speed']!=undefined)?config['speed']:5000);
-		speed = ((this.transition == 'opacity' && speed < 6000)?6000:speed);
+		this.nw_elements_length = this.nw_elements.length;
 
 		var nw_container = document.getElementById(config['id']);
 		nw_container.classList.add("nw-slider");
 
 		var calc_height_by_img = document.querySelector('#' + config['id'] + ' li img');
-		if(calc_height_by_img != null)
-			nw_container.style.height = calc_height_by_img.height + 'px';
-		else
-			nw_container.style.height = config['height'] + 'px';
+		nw_container.style.height = calc_height_by_img.height + 'px';
 
-		nw_container.onmouseover = function(){self.pause()}
-		nw_container.onmouseout = function(){self.restart()}
+		nw_container.onmouseover = function(){transition_success = false;}
+		nw_container.onmouseout = function(){transition_success = true;}
 
-		if(config['buttons'] != undefined && config['buttons'] == true){
+		set_vars(config);
+		if(config['buttons'] != undefined && config['buttons'] == true)
+			add_buttons(nw_container);
 
-			this.add_buttons(nw_container);
-		}
-
-		this.start();
+		prepare();
+		setInterval(prepare, speed);
 	}
 
-	this.prepare = function(){
+	this.changeClass = function(exception){
+
+		for(var i=0 ; i<self.nw_elements_length; i++){
+			if(transition_success === true || exception === true)
+				self.nw_elements[i].className =
+					((i!=self.nw_slider_count)?'nw-slider-'+transition+'-secondary':'nw-slider-'+transition+'-primary');
+		}
+	}
+
+	function add_buttons(nw_container){
+
+		var div = document.createElement('div');
+		div.className = 'nw-slider-arrow-left';
+		nw_container.appendChild(div);
+
+		div.onclick = function(){
+
+			if(self.btnStatus === true) {
+
+				setBtnStatus();
+
+				if(self.first_left == false && self.first_right == false)
+					if(self.nw_slider_count == 1)
+						self.nw_slider_count = self.nw_elements_length - 1;
+					else
+						self.nw_slider_count = ((self.nw_slider_count-2 < 0)?self.nw_elements_length-2:self.nw_slider_count-2);
+				else
+					self.nw_slider_count = ((--self.nw_slider_count < 0)?self.nw_elements_length-1:self.nw_slider_count);
+
+				self.changeClass(true);
+				if(self.first_left == false)self.first_left = true;
+			}
+		}
+
+		var div = document.createElement('div');
+		div.className = 'nw-slider-arrow-right';
+		nw_container.appendChild(div);
+
+		div.onclick = function(){
+
+			if(self.btnStatus === true) {
+
+				setBtnStatus();
+
+				if(self.first_right != false || self.first_left != false)
+					self.nw_slider_count = ((++self.nw_slider_count >= self.nw_elements_length)?0:self.nw_slider_count);
+
+				self.changeClass(true);
+				if(self.first_right == false)self.first_right = true;
+			}
+		}
+	}
+
+	function prepare(){
 
 		self.changeClass();
 
 		if(transition_success == true){
 			if(self.first_left)self.first_left=false;
 			if(self.first_right)self.first_right=false;
-			self.nw_slider_count = ((++self.nw_slider_count >= self.nw_elements.length)?0:self.nw_slider_count);
+			self.nw_slider_count = ((++self.nw_slider_count >= self.nw_elements_length)?0:self.nw_slider_count);
 		}
 	}
 
-	this.changeClass = function(exception){
+	function setBtnStatus(){
 
-		var exception_ = ((exception == undefined)?false:exception);
+		if(self.btnStatus === true && transition != 'none'){
 
-		for(var i=0 ; i<self.nw_elements.length; i++)
-			if(transition_success == true || exception_ == true)
-				self.nw_elements[i].className =
-					((i!=self.nw_slider_count)?'nw-slider-'+self.transition+'-secondary':'nw-slider-'+self.transition+'-primary');
-	}
+			self.btnStatus = false;
+			var x = 0;
 
-	this.start = function(){
+			btnActive = setInterval(function(){
 
-		this.prepare();
-		setInterval(this.prepare, speed);
-	}
+				if(x > 0) {
+					window.clearInterval(btnActive);
+					self.btnStatus = true;
+				}
+				++x;
 
-	this.restart = function(){
-
-		transition_success = true;
-	}
-
-	this.pause = function(){
-
-		transition_success = false;
-	}
-}
-
-nw_slider_lib.prototype.add_buttons = function(nw_container){
-
-	var self = this;
-	var div = document.createElement('div');
-	div.className = 'nw-slider-arrow-left';
-	nw_container.appendChild(div);
-
-	div.onclick = function(){
-
-		if(self.first_left == false && self.first_right == false){
-			self.nw_slider_count = ((self.nw_slider_count-2 < 0)?self.nw_elements.length-1:self.nw_slider_count-2);
+			},500);
 		}
-		else
-			self.nw_slider_count = ((--self.nw_slider_count < 0)?self.nw_elements.length-1:self.nw_slider_count);
-
-		self.changeClass(true);
-		if(self.first_left == false)self.first_left = true;
 	}
 
-	var div = document.createElement('div');
-	div.className = 'nw-slider-arrow-right';
-	nw_container.appendChild(div);
+	function set_vars(config){
 
-	div.onclick = function(){
+		if(config['backgroundColor'] != undefined)
+			for(var i = 0; i < this.nw_elements_length; i++)
+    			this.nw_elements[i].style.backgroundColor = config['backgroundColor'];
 
-		if(self.first_right != false || self.first_left != false)
-			self.nw_slider_count = ((++self.nw_slider_count >= self.nw_elements.length)?0:self.nw_slider_count);
-
-		self.changeClass(true);
-		if(self.first_right == false)self.first_right = true;
+		transition = ((config['transition']!=undefined)?config['transition']:'linear-left');
+		speed = ((config['speed']!=undefined)?config['speed']:5000);
+		speed = ((transition == 'opacity' && speed < 6000)?6000:speed);
+		speed = ((transition != 'none' && speed < 2000)?2000:speed);
 	}
 }
 
@@ -116,9 +139,8 @@ function nw_slider(){
 
 		add_sliders_length = add_sliders.length;
 
-		add_sliders.push(new nw_slider_lib(add_sliders_length));
+		add_sliders.push(new nw_slider_lib());
 		add_sliders[add_sliders_length].run(config_);
-
 	}
 }
 
---
+++ readme.md
@@ -0,0 +1,17 @@
+NW SLIDER
+=========
+
+Nw slider is a responsive carousel very simple yet enough to make an elegant slider of images.
+
+Freatures
+---------
+
+It works with a javascript library with only 2KB in size and does not require jQuery.
+Transitions fall on css styles, yet the nw slider css file weighs just under 4KB.
+The propositios of nw slider are: light and simple.
+Put a nice carousel on your website with only 6KB in size.
+
+Visit our website to know more
+------------------------------
+https://www.newrons.com/plugins-js/carousel-responsive/nw-slider
+Developer by Gilbert López |  newronas.com
